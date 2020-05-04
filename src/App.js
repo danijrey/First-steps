4ce6a156af5a41000d2f74080edf6ce080bfdee4
Daniela <dd.jj.rr@me.com>
2020-05-04 17:01:51 -0500

    Ejercicio uno React

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <h1> Hola soy Dani</h1>
+    <div>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
   );
 }
 
